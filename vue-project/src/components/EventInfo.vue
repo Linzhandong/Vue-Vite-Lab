37b8e8a8bce0577e5b1e74d2eae9a8c8e404ddde
Bragelonne <2338594066@qq.com>
2025-10-20 21:27:05 +0800

    create and update EventInfo.vue

---
+++ vue-project/src/components/EventInfo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Event } from '@/types'
+
+defineProps<{
+  event: Event
+}>()
+</script>
+
+<template>
+  <div class="event-info">
+    <div class="category">{{ event.category }}</div>
+    <div class="organizer">{{ event.organizer }}</div>
+  </div>
+</template>
+
+<style scoped>
+.event-info {
+  font-size: 14px;
+  text-align: right;
+  margin-top: 10px;
+}
+
+.category, .organizer {
+  font-size: 14px;
+}
+</style>
