6e2b6aa4fdbb6b3a83cd468b5557ce29b91eb2f2
Bragelonne <2338594066@qq.com>
2025-10-20 21:05:45 +0800

    change App.vue agant

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,6 +12,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      
     </div>
   </header>
 
