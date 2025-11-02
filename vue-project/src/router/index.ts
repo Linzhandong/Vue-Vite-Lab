3a2ce1626c2b0612962086ca593368201d060e4d
Bragelonne <2338594066@qq.com>
2025-11-02 16:36:07 +0800

    update

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -14,7 +14,7 @@ import EventService from '@/services/EventService'
 import { useEventStore } from '@/stores/event'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),//import.meta.env.BASE_URL
   routes: [
     {
       path: '/',
